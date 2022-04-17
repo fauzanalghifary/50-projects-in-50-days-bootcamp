6f61e0072ae93d7137d9ad5ad1ea3fec6571c082
Fauzan Al Ghifary <m.fauzanghifary@gmail.com>
2022-04-17 17:40:35 +0700

    finish day 15

---
+++ Day-15-incrementing-counter/script.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = 0;
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+    const incremenet = target / 400;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + incremenet)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
